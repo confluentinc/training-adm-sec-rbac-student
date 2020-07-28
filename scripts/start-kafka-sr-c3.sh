4f9643958bb4cbc70cfa2f0f0e6abe9ca241266f
chuck-confluent <chuck@confluent.io>
2020-07-28 13:02:19 -0700

    update shortcut script

--- scripts/start-kafka-sr-c3.sh
+++ scripts/start-kafka-sr-c3.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo "\n############## Starting Apache Directory Studio UI ############\n"
-~/ApacheDirectoryStudio/ApacheDirectoryStudio &
+# Need to first start directory service UI with
+# ~/ApacheDirectoryStudio/ApacheDirectoryStudio &
+# then manually start the directory service in the ApacheDS UI
 
-echo "\n#################### Starting ZK and CP Server ##################\n"
+echo -e "\n#################### Starting ZK and CP Server ##################\n"
 systemctl start confluent-zookeeper confluent-server
 
+while [[ -z $(nc -z -v mds 8090 2>&1 | grep 'open') ]]; do
+    sleep 5
+    echo "waiting for MDS to start"
+done
 
 export KAFKA_CLUSTER_ID=$(curl -k -s https://mds:8090/v1/metadata/id | jq -r .id)
-echo "\nKafka Cluster ID: $KAFKA_CLUSTER_ID\n"
+echo -e "\nKafka Cluster ID: $KAFKA_CLUSTER_ID\n"
 
-echo "\n################ Log into CP ###################\n"
+echo -e "\n################ Log into CP ###################\n"
 expect <<EOF
 spawn confluent login --url https://mds:8090
 expect "Username: "
@@ -21,7 +26,7 @@ expect "Logged in as"
 EOF
 
 ################################### SCHEMA REGISTRY ###################################
-echo "\nCreating role bindings and starting Schema Registry\n"
+echo -e "\nCreating role bindings and starting Schema Registry\n"
 
 # SecurityAdmin on SR cluster itself
 confluent iam rolebinding create \
@@ -43,7 +48,7 @@ done
 systemctl start confluent-schema-registry
 
 ############################## Control Center ###############################
-echo "\nCreating role binding and starting Control Center\n"
+echo -e "\nCreating role binding and starting Control Center\n"
 
 # C3 only needs SystemAdmin on the kafka cluster itself
 confluent iam rolebinding create \
